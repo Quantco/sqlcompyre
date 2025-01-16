45d0647f3a9a90c1f724e1f4a6defa69076ba25b
Oliver Borchert <oliver.borchert@quantco.com>
2025-01-17 00:27:36 +0100

    feat: Add column statistics to query inspection

---
+++ tests/analysis/query_inspection/test_column_stats.py
@@ -0,0 +1,15 @@
+import sqlalchemy as sa
+
+import sqlcompyre as sc
+
+
+def test_column_stats_min(engine: sa.Engine, table_characters: sa.Table):
+    inspection = sc.inspect_table(engine, table_characters)
+    assert inspection.column_stats("age").min == 6
+    assert inspection.column_stats("first_name").min == "Daisy"
+
+
+def test_column_stats_max(engine: sa.Engine, table_characters: sa.Table):
+    inspection = sc.inspect_table(engine, table_characters)
+    assert inspection.column_stats("age").max == 65
+    assert inspection.column_stats("first_name").max == "Scrooge"
